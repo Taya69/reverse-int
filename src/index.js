f702583a9d2a9c1286a980f47c15b03d5dcc27a3
Taya69 <27taya@mail.ru>
2020-10-25 07:11:43 +0300

    feat: solusion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+    let n2 = n;
+  if (n < 0) {n2 = Math.abs(n)}
+  let str = n2.toString();
+  str2 = str.split('').reverse().join('')
   
+  //if (n < 0) {return parseInt(str2)*(-1)}
+  return parseInt(str2)
 }
